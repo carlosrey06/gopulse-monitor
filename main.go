43098fd3d621d663656262caf373bc76c9c242bf
Carlos Reyes <carlosrey1910@gmail.com>
2026-08-22 13:03:07 -0600

    feat: check websites concurrently

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
@@ -34,7 +35,6 @@ func checkWebsite(url string) {
 	fmt.Println("Status code:", response.StatusCode)
 	fmt.Println("Response time:", duration.Milliseconds(), "ms")
 }
-
 func main() {
 	websites := []string{
 		"https://example.com",
@@ -42,7 +42,22 @@ func main() {
 		"https://github.com",
 	}
 
+	start := time.Now()
+
+	var wg sync.WaitGroup
+
 	for _, website := range websites {
-		checkWebsite(website)
+		wg.Add(1)
+
+		go func(url string) {
+			defer wg.Done()
+			checkWebsite(url)
+		}(website)
 	}
+
+	wg.Wait()
+
+	totalDuration := time.Since(start)
+
+	fmt.Println("Total time:", totalDuration.Milliseconds(), "ms")
 }
